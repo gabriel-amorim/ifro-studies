cdbfd74c9a4f4dde8cc17e121e1a37289c6ff165
egitoo <egitoo@gmail.com>
2022-09-23 19:40:20 -0400

    alterações

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import Botao from './components/Botao';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World</p>
+      <Botao/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Formulario extends React.Component {
+    render() {
+        return (
+            <form>
+
+            </form>
+        )
+    }
+}
+
+export default Formulario;
