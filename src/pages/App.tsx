0bee5ea7ff34153c9f6fb69e5f4c3d1a92532830
egitoo <egitoo@gmail.com>
2022-09-23 21:09:07 -0400

    update

---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+
+import Botao from '../components/Botao';
+import Formulario from '../components/Formulario';
+import Lista from '../components/Lista';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World</p>
+      <Formulario/>
+      <Lista/>
+    </div>
+  );
+}
+
+export default App;
